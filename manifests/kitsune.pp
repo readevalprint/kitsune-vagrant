930911584424afc2cc7620cb67714306c0cc8c4d
Tim Watts <tim@readevalprint.com>
2012-01-06 12:25:10 -0200

    rm supervisord settings, added petter bash prompt

--- manifests/kitsune.pp
+++ manifests/kitsune.pp
@@ -26,29 +26,8 @@ DATABASES = {
     require => Exec['git_clone'],
 }
 
-file { '/etc/supervisor/supervisord.conf':
-    content => "
-[unix_http_server]
-file=/var/run//supervisor.sock   ; (the path to the socket file)
-chmod=0700                       ; sockef file mode (default 0700)
-
-[supervisord]
-logfile=/var/log/supervisor/supervisord.log ; (main log file;default $CWD/supervisord.log)
-pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
-childlogdir=/var/log/supervisor            ; ('AUTO' child log dir, default $TEMP)
-
-[rpcinterface:supervisor]
-supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
-
-[supervisorctl]
-serverurl=unix:///var/run//supervisor.sock ; use a unix:// URL  for a unix socket
-
-[program:kitsune]
-command = /home/vagrant/kitsune/manage.py runserver 33.33.33.10:8000
-directory = /home/vagrant/kitsune
-user = vagrant
-",
-    require => Exec['db_import'],
+file { '/home/vagrant/.bash_profile':
+    content => 'PS1="\[\e[32m\][\u@\h]\[\e[0m\]\[\e[37m\][\t]\[\e[0m\]\n\[\e[36m\][\w]\[\e[0m\]>"'
 }
 
 $packages_native = [
@@ -82,7 +61,7 @@ exec { "packages_update":
 
 
 exec { "git_clone":
-    command => "git clone git://github.com/mozilla/kitsune.git",
+    command => "git clone git://github.com/mozilla/kitsune.git || echo $?",
     path => "/bin:/sbiny:/usr/bin:/usr/local/sbin:/usr/sbin",
     cwd => "/home/vagrant",
     logoutput => "true",
@@ -118,14 +97,14 @@ exec { "packages_vendor":
 }
 
 exec { "db_create":
-    command => "mysqladmin -u root create kitsune;",
+    command => "mysqladmin -u root create kitsune || echo $?",
     logoutput => "true",
     path => "/usr/bin:/bin",
     require => Exec['packages_vendor'],
 }
 
 exec { "db_import":
-    command => "mysql -u root kitsune < scripts/schema.sql;",
+    command => "mysql -u root kitsune < scripts/schema.sql || echo $?",
     cwd => "/home/vagrant/kitsune",
     logoutput => "true",
     path => "/usr/bin:/bin",
@@ -134,20 +113,8 @@ exec { "db_import":
 
 exec { "sql_migrate":
     command => "python /home/vagrant/kitsune/vendor/src/schematic/schematic /home/vagrant/kitsune/migrations/",
+    logoutput => "true",
     path => "/usr/bin:/bin",
     require => Exec["db_import"],
 }
 
-exec { "supervisor_stop":
-    command => "/etc/init.d/supervisor stop",
-    logoutput => "true",
-    path => "/usr/bin:/bin",
-    require => file['/etc/supervisor/supervisord.conf'],
-}
-
-exec { "supervisor_start":
-    command => "/etc/init.d/supervisor start",
-    logoutput => "true",
-    path => "/usr/bin:/bin",
-    require => Exec['supervisor_stop'],
-}
